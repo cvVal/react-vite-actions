f78033e0fd00405fb43d80d9bc059bc32bd8877f
sev-valdez <severianovaldez@lithia.com>
2023-03-28 00:00:08 -0400

    Add gh pages base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://cvval.github.io/react-vite-actions/',
   plugins: [react()],
 })
